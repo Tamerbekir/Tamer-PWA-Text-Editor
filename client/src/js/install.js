b3f38437fc801fe811fd7f22db68f527bdf0db13
Tamerbekir <astoryofame@gmail.com>
2024-04-27 19:37:05 -0400

    Added install logic for PWA

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,35 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    //preventing default
+    event.preventDefault();
+    // Store the triggered events
+    // This allows users to install the PWA / App
+    window.deferredPrompt = event;
+    // Store the triggered events
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        return
+    }
+
+    // Show prompt to user when they click on the download button
+    promptEvent.prompt()
+    // Reset the deferred prompt variable, it can only be used once.
+    window.deferredPrompt = null
+
+    butInstall.classList.toggle('hidden', true)
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // Clears prompt
+    window.deferredPrompt = null
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file. ✅
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: Add CSS loaders and babel to webpack.✅
 
 module.exports = () => {
   return {
