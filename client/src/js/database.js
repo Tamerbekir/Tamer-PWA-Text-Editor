054daca6618e89d9200891052172e880c87f70c2
Tamerbekir <astoryofame@gmail.com>
2024-04-27 18:28:12 -0400

    Added plugins to and loaders to webpack. Added export to database that accepts content

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,30 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// TODO: Add logic to a method that accepts some content and adds it to the database 
+// Exporting a function that takes in data and updates it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
+
+console.log('Post to the database')
+
+// Creating a connection to the database 'jate' and version we want to use (1)
+const contactDb = await openDB('jate', 1);
+
+// Create a new transaction and specify the database and data privileges.
+//Transaction method used to create and manage a transaction within a database
+const tx = contactDb.transaction('jate', 'readwrite')
+
+// Open up the desired object store.
+const store = tx.objectStore('jate')
+
+// Using the put method to update the database with the ID of the content and what the text/content itself
+const request = store.put({ id: 1, value: content })
+
+// Get confirmation of the request.
+const result = await request;
+console.log('Content saved to database', result)
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add and configure workbox plugins for a service worker and manifest file. ✅
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -18,12 +18,54 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({ //adding HTML webpack plugin
+        template: './index.html',  //using template HTML from client directory
+        title: 'Text Editor' // title of the page
+      }),
+      new InjectManifest({ // adding InjectManifest plugin
+        swSrc: './src-sw.js', // Adding service worker from Client directory
+        swDest: 'src-sw.js' // Adding Service working destination to dist directory
+      }),
+      // adding webpackPWA manifest and adding properties to manifest
+      new WebpackPwaManifest({
+        fingerprint: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'Text Editor',
+        description: 'Create, write and edit your text right!',
+        background_color: '#376191',
+        theme_color: '#CBD1D7',
+        start_url: './',
+        publicPath: './',
+        //bringing in icons from client directory along with all file sizes and joining them,
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+      })
     ],
 
+  //brining in CSS loaders and babel-loader for ES6
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          }
+        }
       ],
     },
   };
